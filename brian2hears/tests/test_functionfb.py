12d63bef4ee8f6bf5c0f0d881494ad2787830f80
Victor Benichoux <victor.benichoux@ens.fr>
2014-06-11 13:49:56 +0200

    formatted tests

--- brian2hears/tests/test_functionfb.py
+++ brian2hears/tests/test_functionfb.py
@@ -2,22 +2,25 @@ from brian2 import *
 from brian2hears import *
 import time
 
-cf = erbspace(10*Hz, 1000*Hz, 16)
-signal = np.random.randn(1000)
-sound = TimedArray(signal, dt = defaultclock.dt)
 
-iir_filter = GammatoneFilterbank(sound, cf)
-func = FunctionFilterbank(iir_filter, 'clip(x, 0, Inf)')
-M = StateMonitor(func, 'out', record = True)
+def test_functionfb():
+    cf = erbspace(10*Hz, 1000*Hz, 16)
+    signal = np.random.randn(1000)
+    sound = TimedArray(signal, dt = defaultclock.dt)
 
-iir_net = Network(iir_filter, M, func)
-t0 = time.time()
-iir_net.run(len(signal)*defaultclock.dt)
-print 'IIR in ', time.time()-t0
-iir_out = M.out_.T
+    iir_filter = GammatoneFilterbank(sound, cf)
+    func = FunctionFilterbank(iir_filter, 'clip(x, 0, Inf)')
+    M = StateMonitor(func, 'out', record = True)
 
+    iir_net = Network(iir_filter, M, func)
+    t0 = time.time()
+    iir_net.run(len(signal)*defaultclock.dt)
+    print 'IIR in ', time.time()-t0
+    iir_out = M.out_.T
 
-plot(iir_out[:,::2])
+    assert (iir_out >=0).all()
+    #plot(iir_out[:,::2])
+    #show()
 
-show()
-
+if __name__ == '__main__':
+    test_functionfb()
--- brian2hears/tests/test_restructure.py
+++ brian2hears/tests/test_restructure.py
@@ -2,31 +2,30 @@ from brian2 import *
 from brian2hears import *
 import time
 
-cf = erbspace(10*Hz, 1000*Hz, 32)
-signal = np.random.randn(1000)
-sound = TimedArray(signal, dt = defaultclock.dt)
+def test_indexing():
+    cf = erbspace(10*Hz, 1000*Hz, 32)
+    signal = np.random.randn(1000)
+    sound = TimedArray(signal, dt = defaultclock.dt)
 
-indices = np.zeros(2*len(cf), dtype = int)
-indices[::2] = np.arange(len(cf))
-indices[1::2] = np.arange(len(cf))
+    indices = np.zeros(2*len(cf), dtype = int)
+    indices[::2] = np.arange(len(cf))
+    indices[1::2] = np.arange(len(cf))
 
-iir_filter = GammatoneFilterbank(sound, cf)
-# multiplexed = NeuronGroup(2*len(cf), 'x_indices : integer')
-# multiplexed.variables.add_reference('x', iir_filter.variables['out'], index = 'x_indices')
-# multiplexed.x_indices = indices
-multiplexed = IndexedFilterbank(iir_filter, indices)
-Miir = StateMonitor(multiplexed, 'out', record = True)
+    iir_filter = GammatoneFilterbank(sound, cf)
+    # multiplexed = NeuronGroup(2*len(cf), 'x_indices : integer')
+    # multiplexed.variables.add_reference('x', iir_filter.variables['out'], index = 'x_indices')
+    # multiplexed.x_indices = indices
+    multiplexed = IndexedFilterbank(iir_filter, indices)
+    Miir = StateMonitor(multiplexed, 'out', record = True)
 
-iir_net = Network(iir_filter, Miir)
-t0 = time.time()
-iir_net.run(len(signal)*defaultclock.dt)
-print 'IIR in ', time.time()-t0
-iir_out = Miir.out_.T
+    iir_net = Network(iir_filter, Miir)
+    t0 = time.time()
+    iir_net.run(len(signal)*defaultclock.dt)
+    print 'IIR in ', time.time()-t0
+    iir_out = Miir.out_.T
 
-subplot(211)
-plot(iir_out[:,::2])
-subplot(212)
-plot(iir_out[:,1::2])
+    assert (iir_out[:,1::2] == iir_out[:,::2]).all()
 
-show()
 
+if __name__ == '__main__':
+    test_indexing()
