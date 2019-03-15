3e95d65f49c00a5afaa8a43346fd2cb4c2ae26ba
thesamovar <dg.github@thesamovar.net>
2019-03-15 16:06:51 +0000

    More sounds

--- debugging/testsounds.py
+++ debugging/testsounds.py
@@ -2,7 +2,7 @@ from brian2 import *
 from brian2hears.sounds import *
 import numpy
 
-# s = tone([1]*kHz, 10*ms)
+s1 = tone([1]*kHz, 100*ms)
 # s = harmoniccomplex(1*kHz, 10*ms, phase=[0, 0, 0])
 # s = whitenoise(10*ms)
 # s = powerlawnoise(10*ms, 2.0)
@@ -11,6 +11,11 @@ import numpy
 # s = irns(1*ms, 0.9, 5, 10*ms)
 # s = irno(1*ms, 0.9, 5, 10*ms)
 # s = click(10*ms)
-s = clicks(1*ms, 5, 2*ms)
+# s = clicks(1*ms, 5, 2*ms)
+# s = silence(10*ms)
+s2 = vowel('a', duration=100*ms)
+#s = sequence([s1, s2])
+s = s1+s2
+s.repeat(10).play(normalise=True)
 plot(s)
 show()
