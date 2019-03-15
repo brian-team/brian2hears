d54345f63e461a52ad64499e1b57affd7a42566d
thesamovar <dg.github@thesamovar.net>
2019-03-15 17:57:18 +0000

    plotting and prefs done - just filtering and hrtf to go

--- brian2hears/erb.py
+++ brian2hears/erb.py
@@ -22,7 +22,7 @@ def erbspace(low, high, N, earQ=9.26449, minBW=24.7, order=1):
     cf = -(earQ * minBW) + exp((arange(N)) * (-log(high + earQ * minBW) + \
             log(low + earQ * minBW)) / (N-1)) * (high + earQ * minBW)
     cf = cf[::-1]
-    return cf
+    return cf*Hz
 
 # Testing
 if __name__ == '__main__':
