93e710f8e5f147f8e8689297370f5e308eb0f87c
thesamovar <dg.github@thesamovar.net>
2019-03-29 12:22:48 +0000

    Allow limited store/restore and check for standalone mode

--- brian2hears/tests/testfilterbank.py
+++ brian2hears/tests/testfilterbank.py
@@ -114,6 +114,55 @@ def test_zhang_synapse():
     # show()
 
 
+def test_filterbankgroup_restart():
+    sound1 = tone(1 * kHz, .1 * second)
+    sound2 = whitenoise(.1 * second)
+    sound = sound1 + sound2
+    sound = sound.ramp()
+    cf = erbspace(20 * Hz, 20 * kHz, 3000)
+    cochlea = Gammatone(sound, cf)
+    # Half-wave rectification and compression [x]^(1/3)
+    ihc = FunctionFilterbank(cochlea, lambda x: 3 * clip(x, 0, Inf) ** (1.0 / 3.0))
+    # Leaky integrate-and-fire model with noise and refractoriness
+    eqs = '''
+    dv/dt = (I-v)/(1*ms)+0.2*xi*(2/(1*ms))**.5 : 1 (unless refractory)
+    I : 1
+    '''
+    anf = FilterbankGroup(ihc, 'I', eqs, reset='v=0', threshold='v>1', refractory=5*ms, method='euler')
+    M = SpikeMonitor(anf)
+    store()
+    run(sound.duration)
+    restore()
+    run(sound.duration)
+    # plot(M.t/ms, M.i, ',k')
+    # show()
+
+# This test doesn't pass yet because some work is needed to make store/restore work for anything except restart to 0
+# def test_filterbankgroup_store_restore():
+#     sound1 = tone(1 * kHz, .1 * second)
+#     sound2 = whitenoise(.1 * second)
+#     sound = sound1 + sound2
+#     sound = sound.ramp()
+#     cf = erbspace(20 * Hz, 20 * kHz, 3000)
+#     cochlea = Gammatone(sound, cf)
+#     # Half-wave rectification and compression [x]^(1/3)
+#     ihc = FunctionFilterbank(cochlea, lambda x: 3 * clip(x, 0, Inf) ** (1.0 / 3.0))
+#     # Leaky integrate-and-fire model with noise and refractoriness
+#     eqs = '''
+#     dv/dt = (I-v)/(1*ms)+0.2*xi*(2/(1*ms))**.5 : 1 (unless refractory)
+#     I : 1
+#     '''
+#     anf = FilterbankGroup(ihc, 'I', eqs, reset='v=0', threshold='v>1', refractory=5*ms, method='euler')
+#     M = SpikeMonitor(anf)
+#     run(sound.duration/2)
+#     store()
+#     run(sound.duration / 2)
+#     restore()
+#     run(sound.duration / 2)
+#     plot(M.t/ms, M.i, ',k')
+#     show()
+
+
 if __name__=='__main__':
     # test_basic_filterbanks()
     # test_filterbankgroup()
@@ -124,5 +173,6 @@ if __name__=='__main__':
     # test_drnl()
     # test_tan_carney()
     #BrianLogger.log_level_diagnostic()
-    test_zhang_synapse()
-
+    # test_zhang_synapse()
+    test_filterbankgroup_restart()
+    # test_filterbankgroup_store_restore()
