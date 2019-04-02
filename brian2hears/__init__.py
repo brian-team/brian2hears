e584ab79b6bb0a95000316515d63631fb888c4e2
Marcel Stimberg <marcel.stimberg@inserm.fr>
2019-04-02 15:31:27 +0200

    Use a single source to store the package version

--- brian2hears/__init__.py
+++ brian2hears/__init__.py
@@ -1,11 +1,15 @@
+import os
+
 from .prefs import *
 from .bufferable import *
 from .sounds import *
-#from .onlinesounds import *
 from .erb import *
 from .filtering import *
 from .hrtf import *
 from .db import *
 from .plotting import *
 
-__version__ = '0.9'
+with open(os.path.join(os.path.dirname(__file__),
+                       '..',
+                       'VERSION')) as version_file:
+    __version__ = version_file.read().strip()
--- setup.py
+++ setup.py
@@ -1,11 +1,29 @@
 from setuptools import setup, find_packages
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as version_file:
+    version = version_file.read().strip()
 
 setup(
     name="brian2hears",
-    version="0.9",
+    version=version,
     author="Bertrand Fontaine, Dan Goodman, Marcel Stimberg, Victor Benichoux, Romain Brette",
     author_email="team@briansimulator.org",
     description="Auditory modelling package for brian2 simulator",
     install_requires=['numpy', 'scipy', 'brian2', 'six', 'future'],
     packages=find_packages(),
+    use_2to3=False,
+    zip_safe=False,
+    license='CeCILL-2.1',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: CEA CNRS Inria Logiciel Libre License, version 2.1 (CeCILL-2.1)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Bio-Informatics'
+    ]
 )
